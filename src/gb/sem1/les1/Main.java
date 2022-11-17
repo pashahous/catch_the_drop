b3c4ed174cbb161b5308ac2e3cf22bce36a3ddaa
p <pashahous1@rambler.ru>
2022-11-17 17:58:55 +0300

    First semestr homework 1

--- src/gb/sem1/les1/Main.java
+++ src/gb/sem1/les1/Main.java
@@ -1,5 +1,7 @@
 package gb.sem1.les1;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 /*1. Создать пустой проект в IntelliJ IDEA и прописать метод main().
 2. Создать переменные всех пройденных типов данных и инициализировать их значения.
 3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
@@ -17,9 +19,17 @@ package gb.sem1.les1;
 является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.*/
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Task3(2, 3, 5, 6.5f));
+        System.out.println(Task4(10, 15));
+        System.out.println(Task5(10));
+        System.out.println(Task6(-10));
+        System.out.println(Task7("Dimooon"));
+        System.out.println(Task8IsHighYear(1900));
+        System.out.println(Task8IsHighYear(1992));
 
     }
-    private static void Task2(){
+
+    private static void Task2() {
         byte b = 5; //2**8 -128:127
         short s = 1534;//2**16 -32768:32767
         int i = 12345; //2**32 -2147483648 2147483647
@@ -29,8 +39,40 @@ public class Main {
 
     }
 
-    private static int Task3( double a,double b,double c,double d){
+    private static float Task3(float a, float b, float c, float d) {
 
-        return (int)(a * (b + (c / d)));
+        return a * (b + (c / d));
+    }
+
+    private static boolean Task4(float a, float b) {
+        float res = a + b;
+        if (10 <= res && res <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static String Task5(int a) {
+        if (a >= 0) {
+            return "Number is positive +";
+        } else return "Number is negative - ";
+
+    }
+
+    private static boolean Task6(int a) {
+        return a < 0;
+    }
+
+    private static String Task7(String name) {
+        return "Привет " + name;
+
+    }
+
+    private static boolean Task8IsHighYear(int year) {
+        //4,100,400
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
 }
+
+
