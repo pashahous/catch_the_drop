fc7c4f26622b11e656b979f9e1f1a1fa5864ad40
p <pashahous1@rambler.ru>
2022-11-17 18:12:42 +0300

    First semestr homework 2

---
+++ src/gb/sem1/les2/Main.java
@@ -0,0 +1,20 @@
+package gb.sem1.les2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(testCase(5));
+
+    }
+    public static String testCase(int num){
+        switch(num){
+            case 5:
+                return "FIve";
+
+            case 1:
+                return "One";
+
+            default: return "Nothibg";
+                }
+    }
+
+}
